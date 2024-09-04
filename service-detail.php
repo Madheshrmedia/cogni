3274a2a17370ecf89577a86314ef7127f693c7bc
Muthubala <muthubala894@gmail.com>
2024-09-04 18:11:42 +0530

    service detail page completed for salon

--- service-detail.php
+++ service-detail.php
@@ -29,21 +29,354 @@ include_once "./header.php";
     <h2>FOR MEN GROOMING</h2>
     </div>
  
-  <div class="container">
+  <div class="container" id="cardcontainer">
   <div class="row g-0 text-center">
-  <div class="col-sm-6 col-md-6">.col-sm-6 .col-md-8</div>
-  <div class="col-sm-6 col-md-6">.col-6 .col-md-4</div>
+  <div class="col-sm-6 col-md-5" style="text-align:left">
+    <p>HAIR CUT </p>
+    <p>KIDS CUT (BOYZ) -  (Upto 5 Years)</p>
+    <p>HEAD MASSAGE (30/60 Minutes)</p>
+    <p>FASHION COLOURING</p>
+    <p>DETAN</p>
+  </div>
+  <div class="col-sm-6 col-md-2"></div>
+  <div class="col-sm-6 col-md-5" style="text-align:left">
+
+    <p>CREATIVE CUT ( STYLING)
+    </p>
+    <p>BEARD DESIGN
+
+    </p>
+    <p>GLOBAL COLOURING
+
+
+</p>
+<p>STREAKS
+
+
+</p>
+  </div>
 </div>
   </div>
+  <div class="button">
+  <button class="book" style="border: none;">BOOK NOW</button>
   </div>
+ 
+  </div>
+   <!-- === -->
+   <div class="cardbig">
+    <div class="heading">
+    <h2>HAIR </h2>
+    </div>
+ 
+  <div class="container" id="cardcontainer">
+  <div class="row g-0 text-center">
+  <div class="col-sm-6 col-md-5" style="text-align:left">
+    <p>HAIR BOTOX
+    </p>
+    <p>HAIR REBONDING
+    </p>
+    <p>STRAIGHTENING
+    </p>
+    <p>ANTI DANDRUFF TREATMENT
+    </p>
+    <p>CUTICLE REPAIR
+    </p>
+  </div>
+  <div class="col-sm-6 col-md-2"></div>
+  <div class="col-sm-6 col-md-5" style="text-align:left">
+
+    <p>KERATIN THEATMENT
+
+    </p>
+    <p>SMOOTHENING
+
+
+    </p>
+    <p>HAIR SPA
+
+
+
+</p>
+<p>HAIR REVIVAL - HAIR FALL CONTROL
+
+
+
+</p>
+  </div>
+</div>
+  </div>
+  <div class="button">
+  <button class="book" style="border: none;">BOOK NOW</button>
+  </div>
+ 
+  </div>
+   <!-- === -->
+   <div class="cardbig">
+    <div class="heading">
+    <h2>SKIN (Facials)</h2>
+    </div>
+ 
+  <div class="container" id="cardcontainer">
+  <div class="row g-0 text-center">
+  <div class="col-sm-6 col-md-5" style="text-align:left">
+    <p>DE TAN FACIAL 
+    </p>
+    <p>SKIN TIGHTENING FACIAL 
+    </p>
+    <p>UNDER EYE DARK CIRCLE TREATMENT
+    </p>
+    <p>ULTRA BRIGHT - ULTIMATE WHITENING & GLOW
+    </p>
+    <p>OXYGENEO-POLLOGEN FACIAL
+    </p>
+  </div>
+  <div class="col-sm-6 col-md-2"></div>
+  <div class="col-sm-6 col-md-5" style="text-align:left">
+
+    <p>24 K GOLD FACIAL 
+
+    </p>
+    <p>YOUTH REVIVAL FACIAL
+
+
+    </p>
+    <p>DEFINE - SKIN FIRMING & TIHGTENING              
+
+
+
+</p>
+<p>HYDRA FACIAL 11 IN ONE 
+
+
+
+</p>
+  </div>
+</div>
+  </div>
+  <div class="button">
+  <button class="book" style="border: none;">BOOK NOW</button>
+  </div>
+ 
+  </div>
+   <!-- === -->
+   <div class="cardbig">
+    <div class="heading">
+    <h2>FOR WOMEN GROOMING</h2>
+    </div>
+ 
+  <div class="container" id="cardcontainer">
+  <div class="row g-0 text-center">
+  <div class="col-sm-6 col-md-5" style="text-align:left">
+    <p>HAIR - FRINGE CUT
+    </p>
+    <p>CREATIVE CUT (STYLING)
+    </p>
+    <p>COLOURING-ROOT TOUCH UP
+    </p>
+    <p>FASHION COLOURING
+    </p>
+  </div>
+  <div class="col-sm-6 col-md-2"></div>
+  <div class="col-sm-6 col-md-5" style="text-align:left">
+
+    <p>HAIR TRIM + HAIR WASH 
+
+    </p>
+    <p>KIDS CUT (GIRLZ) -  (Upto 5 Years)
+
+
+    </p>
+    <p>GLOBAL COLORING
+</p>
+<p>STREAKX
+
+
+
+</p>
+  </div>
+</div>
+  </div>
+  <div class="button">
+  <button class="book" style="border: none;">BOOK NOW</button>
+  </div>
+ 
+  </div>
+   <!-- === -->
+   <div class="cardbig">
+    <div class="heading">
+    <h2>HAIR</h2>
+    </div>
+ 
+  <div class="container" id="cardcontainer">
+  <div class="row g-0 text-center">
+  <div class="col-sm-6 col-md-5" style="text-align:left">
+    <p>HAIR WASH & OUT CURLS
+
+    </p>
+    <p>FLAT IRONING
+
+    </p>
+  
+    <p>HAIR BOTOX
+
+    </p>
+    <p>HAIR REBONDING
+
+    </p>
+    <p>STRAIGHTENING
+    </p>
+    <p>ANTI DANDRUFF TREATMENT
+
+    </p>
+    <p>CUTICLE REPAIR
+
+
+    </p>
+  </div>
+  <div class="col-sm-6 col-md-2"></div>
+  <div class="col-sm-6 col-md-5" style="text-align:left">
+
+    <p> TONGS
+
+
+    </p>
+    <p>HEAD MASSAGE (30/60 Minutes)
+
+
+
+    </p>
+    <p>KERATIN TREATMENT
+</p>
+<p>SMOOTHENING</p>
+<p>HAIR SPA
+</p>
+<p>HAIR REVIVAL - HAIR FALL CONTROL
+
+</p>
+  </div>
+</div>
+  </div>
+  <div class="button">
+  <button class="book" style="border: none;">BOOK NOW</button>
+  </div>
+ 
+  </div>
+   <!-- === -->
+   <div class="cardbig">
+    <div class="heading">
+    <h2>SKIN (Facial)</h2>
+    </div>
+ 
+  <div class="container" id="cardcontainer">
+  <div class="row g-0 text-center">
+  <div class="col-sm-6 col-md-5" style="text-align:left">
+    <p>DE TAN FACIAL 
+    </p>
+    <p>24 K GOLD FACIAL 
+    </p>
+    <p>YOUTH REVIVAL FACIAL
+    </p>
+    <p>HYDRA FACIAL
+    </p>
+    <p>DETAN</p>
+    <p>WAXING 
+    </p>
+  </div>
+  <div class="col-sm-6 col-md-2"></div>
+  <div class="col-sm-6 col-md-5" style="text-align:left">
+
+    <p>MATT IT - PURIFY FOR  ACNE SKIN 
+
+    </p>
+    <p>SKIN TIGHTENING FACIAL 
+
+
+    </p>
+    <p>UNDER EYE DARK CIRCLE TREATMENT
+</p>
+<p>OXYGENEO-POLLOGEN FACIAL
+</p>
+<p>THREADING 
+</p>
+<p>FOOT & HAND SPA (PEDICURE & MANICURE)
+</p>
+  </div>
+</div>
+  </div>
+  <div class="button">
+  <button class="book" style="border: none;">BOOK NOW</button>
+  </div>
+ 
+  </div>
+   <!-- === -->
+   <div class="cardbig">
+    <div class="heading">
+    <h2>BRDIAL SERVICES </h2>
+    </div>
+ 
+  <div class="container" id="cardcontainer">
+  <div class="row g-0 text-center">
+  <div class="col-sm-6 col-md-5" style="text-align:left">
+    <p>BRIDAL - MAKEOVER 
+    </p>
+    
+  </div>
+  <div class="col-sm-6 col-md-2"></div>
+  <div class="col-sm-6 col-md-5" style="text-align:left">
+
+    <p>BRIDE GROOM MAKE OVER
+
+    </p>
+   
+
+
+</p>
+  </div>
+</div>
+  </div>
+  <div class="button">
+  <button class="book" style="border: none;">BOOK NOW</button>
+  </div>
+ 
+  </div>
+  <div class="container">
+  <p>Come and experience the transformation. Let us help you look and feel your best every day!</p>
+  </div>
+
 <style>
+  
     .heading{
         text-align:center;
         background-color: #F1FFF3;
         padding:1rem 0;
+        margin-bottom: 1rem;
+
     }
     .heading h2{
         margin-bottom:0 !important;
+    }
+    #cardcontainer{
+        max-width: 1100px;
+    }
+    #cardcontainer p{
+        padding:1rem;
+        border-bottom: 1px solid #9BB7BF;
+    }
+    .button{
+        text-align:center;
+        margin:3rem auto 5rem auto;
+    }
+   .book {
+        background: linear-gradient(to right, 
+    #D4A200 0%,    /* Stop at 0% */
+    #FFD13D 43%,   /* Stop at 43% */
+    #D6A608 75%,   /* Stop at 75% */
+    #FFC300 100%   /* Stop at 100% */
+);
+border: none;
+padding:10px 36px;
+color: #fff;
+font-size:13px;
     }
 </style>
 
